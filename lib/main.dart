d994de781acea9b94c674b9bebb851420826b477
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-07 22:48:24 +0700

    Column & row

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,84 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Belajar Guys"),
         ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 200,
-                height: 100,
-                child: Text(
-                  "sebenernya saya capek, tapi yaa gimana lagi, terpaksa harus belajar ini",
-                  maxLines: 2,
-                  overflow: TextOverflow.clip,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white, fontStyle: FontStyle.italic),
-                ))),
+        body: Column(
+          // Column mengatur widget tersusun menumpuk dari atas ke bawah, kalo mau menyamping pakainya Row
+          mainAxisAlignment:
+              MainAxisAlignment.center, // mengatur dari atas ke tengah
+          crossAxisAlignment: CrossAxisAlignment.start, // rata kiri
+          children: [
+            Center(
+                child: Container(
+                    color: Colors.lightBlue,
+                    width: 200,
+                    height: 50,
+                    child: Text(
+                      "sebenernya saya capek, tapi yaa gimana lagi, terpaksa harus belajar ini",
+                      maxLines: 2,
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white, fontStyle: FontStyle.italic),
+                    ))),
+            Center(
+                child: Container(
+                    color: Colors.lightBlue,
+                    width: 200,
+                    height: 50,
+                    child: Text(
+                      "sebenernya saya capek, tapi yaa gimana lagi, terpaksa harus belajar ini",
+                      maxLines: 2,
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white, fontStyle: FontStyle.italic),
+                    ))),
+            Center(
+                child: Container(
+                    color: Colors.lightBlue,
+                    width: 200,
+                    height: 50,
+                    child: Text(
+                      "sebenernya saya capek, tapi yaa gimana lagi, terpaksa harus belajar ini",
+                      maxLines: 2,
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white, fontStyle: FontStyle.italic),
+                    ))),
+            Row(
+              children: [
+                Container(
+                  color: Colors.blueGrey,
+                  width: 70,
+                  height: 50,
+                  child: Text(
+                    "Oi",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  color: Colors.blueGrey,
+                  width: 70,
+                  height: 50,
+                  child: Text(
+                    "Oi",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  color: Colors.blueGrey,
+                  width: 70,
+                  height: 50,
+                  child: Text(
+                    "Oi",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
