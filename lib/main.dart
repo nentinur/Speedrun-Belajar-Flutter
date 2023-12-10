7591b773dc5406e579927141a9b4d2112598447e
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-10 11:13:11 +0700

    flexible layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,51 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Animated Container"),
+          title: Text("Flexible Layout"),
         ),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {});
-            },
-            child: AnimatedContainer(
-              color: Color.fromARGB(255, random.nextInt(256),
-                  random.nextInt(256), random.nextInt(256)),
-              duration: Duration(seconds: 1),
-              width: 50.0 + random.nextInt(101),
-              height: 50.0 + random.nextInt(101),
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.amber,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.teal,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.blue,
+                      )),
+                ],
+              ),
             ),
-          ),
+            Flexible(
+                flex: 2,
+                child: Container(
+                  color: Colors.blueGrey,
+                )),
+            Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.brown,
+                )),
+          ],
         ),
       ),
     );
