a4e8bf7c7390953bd8c8b5a49a8c6f68ff9f56c9
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-24 14:15:27 +0700

    decoration text field

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,28 @@ class _MyAppState extends State<MyApp> {
                 // obscureText: true, =>> untuk membuat tampilan text seperti password
                 // maxLength: 5, =>> untuk membatasi jumlah input (contoh: hanya bisa menulis 5 karakter)
                 // maxLines: 1, =>> jumlah maksimal baris
+                decoration: InputDecoration(
+                    fillColor: const Color.fromARGB(255, 203, 230, 243),
+                    filled: true,
+                    icon: Icon(Icons.adb),
+
+                    /// prefix di awal, sufix di akhir
+                    // prefix: Container( =>> bisa diisi apa aja, kalo pake ini gaboleh pake prefixText
+                    //   width: 5, height: 5, color: Colors.amber,
+                    // ),
+                    // suffix: Container( =>> bisa diisi apa aja, kalo pake ini gaboleh pake sufixText
+                    //   width: 5, height: 5, color: Colors.amber,
+                    // ),
+                    prefixIcon: Icon(Icons.person),
+                    prefixText: "Name: ",
+                    prefixStyle: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.w600),
+                    labelText: "Nama Lengkap: ",
+                    // labelStyle: TextStyle(), =>> untuk menghias label
+                    hintText: "nama lengkapnya loh",
+                    // hintStyle: TextStyle(), =>> untuk menghias hint
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
                 onChanged: (value) {
                   setState(() {});
                 },
