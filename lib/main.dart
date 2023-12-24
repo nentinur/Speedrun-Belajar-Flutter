5d10a2565fa35f0fe8c003fe375c1d55164ec3cf
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-24 13:00:01 +0700

    navigasi/multipage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'dart:math';
 
+import 'package:belajar_flutter/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginPage());
+  }
 }
