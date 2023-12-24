c6f3d8cf78f1ede65e98ee83fa4a4061709b70ee
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-24 13:36:38 +0700

    Appbar gradasi, icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-
-import 'package:belajar_flutter/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +9,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage());
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "AppBar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.settings),
+                color: Colors.white),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.exit_to_app),
+                color: Colors.white)
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                    image: AssetImage("assets/pattern.webp"),
+                    fit: BoxFit.none,
+                    repeat: ImageRepeat.repeat)),
+          ),
+        ),
+      ),
+    );
   }
 }
