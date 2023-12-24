92ac8884c02ceefbe30a22268de2b82a1f56b86a
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-12-24 14:32:01 +0700

    media query ( responsive app)

--- lib/main.dart
+++ lib/main.dart
@@ -4,61 +4,51 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController(text: "");
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("text field")),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextField(
-                // obscureText: true, =>> untuk membuat tampilan text seperti password
-                // maxLength: 5, =>> untuk membatasi jumlah input (contoh: hanya bisa menulis 5 karakter)
-                // maxLines: 1, =>> jumlah maksimal baris
-                decoration: InputDecoration(
-                    fillColor: const Color.fromARGB(255, 203, 230, 243),
-                    filled: true,
-                    icon: Icon(Icons.adb),
-
-                    /// prefix di awal, sufix di akhir
-                    // prefix: Container( =>> bisa diisi apa aja, kalo pake ini gaboleh pake prefixText
-                    //   width: 5, height: 5, color: Colors.amber,
-                    // ),
-                    // suffix: Container( =>> bisa diisi apa aja, kalo pake ini gaboleh pake sufixText
-                    //   width: 5, height: 5, color: Colors.amber,
-                    // ),
-                    prefixIcon: Icon(Icons.person),
-                    prefixText: "Name: ",
-                    prefixStyle: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.w600),
-                    labelText: "Nama Lengkap: ",
-                    // labelStyle: TextStyle(), =>> untuk menghias label
-                    hintText: "nama lengkapnya loh",
-                    // hintStyle: TextStyle(), =>> untuk menghias hint
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
-              ),
-              Text(controller.text)
-            ],
-          ),
-        ),
-      ),
+      home: MainPage(),
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Media Query")),
+      body: (MediaQuery.of(context).orientation == Orientation.portrait)
+          ? Column(
+              children: generateContainers(),
+            )
+          : Row(
+              children: generateContainers(),
+            ),
+    );
+  }
+
+  List<Widget> generateContainers() {
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.green,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100,
+      )
+    ];
   }
 }
